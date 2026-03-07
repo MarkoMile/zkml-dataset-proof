617e7c890da2de67584242b2a8aa6c34acc3801c
Marko Milenkovic <mmarkomile@gmail.com>
2026-03-07 01:37:12 +0100

    fix potential bugs in script

--- web-app/scripts/prove.sh
+++ web-app/scripts/prove.sh
@@ -1,12 +1,14 @@
+export PATH="$HOME/.nargo/bin:$PATH"
+
 cd circuits
 
-rm -r ./target
+rm -rf ./target
 
 nargo execute
 
-bb prove -b ./target/circuits.json -w ./target/circuits.gz -o ./target
+npx @aztec/bb.js write_vk_ultra_honk -b ./target/circuits.json -o ./target/vk
 
-bb write_vk -b ./target/circuits.json -o ./target
+npx @aztec/bb.js prove_ultra_honk -b ./target/circuits.json -w ./target/circuits.gz -k ./target/vk -o ./target/proof
 
-bb verify -k ./target/vk -p ./target/proof
+npx @aztec/bb.js verify_ultra_honk -k ./target/vk -p ./target/proof && echo "Proof verified successfully"
 # -i ./target/public_inputs OVO JE OBRISANO I SAD RADI
